82d5061ef16d0f911e96830928a550d44e7afff2
Shiwamon <ivanshumkov0707087@gmail.com>
2025-08-25 21:11:48 +0300

    Initial Commit

---
+++ PortfolioApp/SearchVC.swift
@@ -0,0 +1,29 @@
+//
+//  SearchVC.swift
+//  PortfolioApp
+//
+//  Created by Ivan Shumkov on 26.08.2025.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ PortfolioAppTests/PortfolioAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppTests.swift
+//  PortfolioAppTests
+//
+//  Created by Ivan Shumkov on 25.08.2025.
+//
+
+import Testing
+@testable import PortfolioApp
+
+struct PortfolioAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
